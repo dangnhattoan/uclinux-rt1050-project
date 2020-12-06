8eca4b71cd3e63432b82fa108179f820e2a805ed
Toan Dang <dangnhattoan@gmail.com>
2020-12-06 17:05:49 +0800

    Update project using submodules

---
+++ tools/arm-v7-linux-uclibceabi/lib/gcc/arm-v7-linux-uclibceabi/4.7.4/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/build/fdpic/scratch/gcc/configure --prefix=/home/build/fdpic/install --target=arm-v7-linux-uclibceabi --with-gmp=/home/build/fdpic/install_host --with-mpfr=/home/build/fdpic/install_host --with-mpc=/home/build/fdpic/install_host --with-sysroot=/home/build/fdpic/install/sysroot --disable-libgomp --enable-libmudflap --enable-languages=c,c++ --disable-libquadmath --disable-multilib --disable-libstdcxx-pch --enable-threads=posix --with-arch=armv7-m --with-mode=thumb --without-cloog --without-ppl --disable-nls --enable-libstdcxx-time --with-pkgversion='20170818-165657- build on build.emcraft by build' --without-bugurl";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "arch", "armv7-m" }, { "mode", "thumb" }, { "tls", "gnu" } };
